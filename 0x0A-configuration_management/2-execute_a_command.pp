b3639ce14d0054858c74a0e9862a370f95921505
misporey <rechildmisporey@gmail.com>
2022-01-03 06:00:00 -0800

    Added a file

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# kills a process using puppet
+exec { 'killmenow':
+  command  => '/usr/bin/pkill killmenow',
+  provider => 'shell',
+  returns  => [0, 1],
+
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+## Welcome to alx project course 
+* Today we are going to look at configuration management such as puppet
