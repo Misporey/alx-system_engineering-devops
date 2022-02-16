13faaa8659924a43c49e18b090c7436eb5916815
Misporey <85012441+Misporey@users.noreply.github.com>
2022-02-16 20:56:48 +0000

    Create 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# fix a wordpress site with erro in apache2 setting
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
